310c12e269055030f10119cfd4689edf7a36782a
yukke <soh.ishiguro3914@gmail.com>
2013-10-07 00:18:49 +0900

    add render for search result

--- myapp.pl
+++ myapp.pl
@@ -1,12 +1,34 @@
 #!/usr/bin/env perl
+
 use Mojolicious::Lite;
 
-# Documentation browser under "/perldoc"1;2D
 plugin 'PODRenderer';
 
 get '/' => sub {
   my $self = shift;
   $self->render('index');
+};
+
+get '/result' => sub {
+    my $self = shift;
+
+    my $sp = $self->param('sp');
+    my $chr = $self->param('chr');
+    my $region = $self->param('region');
+    
+    if (!$sp or !$chr or !$region) {
+        $self->render(text=>"Error: Value is empty");
+    }
+    if ($sp =~ m/^\w+$/
+        and $chr =~ m/(^chr\d+$)|(^\d+$)|(^[xyXY]{1}$)/
+        and $region =~ m/^\d+$/) {
+        $self->stash(sp => $sp);
+        $self->stash(chr => $chr);
+        $self->stash(region => $region);
+        $self->render();
+    } else {
+        $self->render(text=>"Error: Invald data is given");
+    }
 };
 
 get '/about' => sub {
@@ -31,4 +53,3 @@ get "/contact" => sub {
 
 
 app->start;
-
